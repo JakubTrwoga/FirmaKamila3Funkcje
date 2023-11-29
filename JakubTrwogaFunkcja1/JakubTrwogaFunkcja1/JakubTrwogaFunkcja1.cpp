2f63e65e9fa79167b797cec9dd1d687d9d4a27cb
JakubTrwoga <152401358+JakubTrwoga@users.noreply.github.com>
2023-11-29 14:09:32 +0100

    FUNKCJA 1 - WPROWADZANIE DANYCH SPRZEDAŻOWYCH

---
+++ JakubTrwogaFunkcja1/JakubTrwogaFunkcja1/JakubTrwogaFunkcja1.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int Pobierz_dane(){
+
+	int Total_Sold, Advance;
+
+	cout << "Podaj ilosc sprzedazy wykonanych przez pracownika";
+	cin >> Total_Sold;
+	cout << endl << "Podaj zaliczke pracownika";
+	cin >> Advance;
+
+	cout << "pracownik wykonal:" << Total_Sold << " sprzedazy oraz wzial:" << Advance << " zl zaliczki";
+
+	return 0;
+}
+
+int main() {
+	Pobierz_dane();
+	return 0;
+}
